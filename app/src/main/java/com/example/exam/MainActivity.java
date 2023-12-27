5b64323eec64382bdb4faf2ba6bf64cd0b93a0c8
saif <saifomar1952001@gmail.com>
2023-12-27 20:24:59 +0300

    adding int x to MainActivity.java

--- app/src/main/java/com/example/exam/MainActivity.java
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -21,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
+        int x = 5;
+
     }
 }
